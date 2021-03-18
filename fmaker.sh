03c7285b08ba0607c2582c6ef739e1da4d25b73b
Yenelm <79953147+Yenelm@users.noreply.github.com>
2021-03-18 04:54:38 +0000

    Add files via upload

---
+++ fmaker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -p "Enter the name of the directory you want created: " dirname
+mkdir $dirname
+echo "The $dirname directory has been created"
+
+exit 0
---
+++ fmaker2.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+read -p "Enter the name of the directory you want created: " dirname
+
+if [ -d $dirname ]; then
+    echo "Directory already exists"
+else
+    mkdir $dirname
+    echo "The $dirname directory has been created"
+fi
+
+exit 0
